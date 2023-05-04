36c04fa34d549ad6523f94307c59e0e395d4c4c7
KamilLyczko <kamlyczko@gmail.com>
2023-05-04 19:15:46 +0200

    data load

--- scripts/NumSlotsLMAC/data_definitions.R
+++ scripts/NumSlotsLMAC/data_definitions.R
@@ -1,4 +1,7 @@
 num_slots_LMAC_N10_data <- read.csv("data_sheets/NumSlotsLMAC/NumSlotsLMAC_N10.csv", sep=";")
-#num_slots_LMAC_N10_data <- num_slots_LMAC_N10_data[order(num_slots_LMAC_N10_data$run),]
 run_name <- "NumSlotsLMAC-2-20230503-19:12:14-15424"
-num_slots_LMAC_N10_scalars <- subset(num_slots_LMAC_N10_data, (run == run_name & type == "scalar"))
+N10_run3_stats <- calculate_stats_of_run(num_slots_LMAC_N10_data, run_name)
+scalars <- get_scalars_of_run(num_slots_LMAC_N10_data, run_name) # działa
+packet_sent <- get_column_values(scalars, "packetSent:count") # nie działa
+
+
--- scripts/NumSlotsLMAC/functions.R
+++ scripts/NumSlotsLMAC/functions.R
@@ -1,14 +1,39 @@
-# nie działa jak należy:
-load_num_slots_LMAC_data <- function() {
-  data <- list()
-  N <- seq(10, 100, 10)
-  i <- 0
-  for(n in N) {
-    directory <- "data_sheets/NumSlotsLMAC/"
-    file_name <- paste0("NumSlotsLMAC_N", n, ".csv")
-    full_dir <- paste0(directory, file_name)
-    data[i] <- read.csv(full_dir, sep=";")
-    i <- i + 1
-  }
-  return(data)
+get_data_of_run <- function(data, run_name) {
+  data_subset <- subset(data, (run == run_name))
+  return(data_subset)
+}
+
+get_scalars <- function(data) {
+  scalar_data <- subset(data, (type == "scalar"))
+  return(scalar_data)
+}
+
+get_scalars_of_run <- function(data, run_name) {
+  data_subset <- get_data_of_run(data, run_name)
+  scalar_data <- get_scalars(data_subset)
+  return(scalar_data)
+}
+
+get_column_values <- function(scalar_data, column_name) {
+  data <- subset(scalar_data, (name == column_name))
+  values_vector <- c(data$values)
+  return(values_vector)
+}
+
+calculate_stats_of_run <- function(data, name_of_run) {
+  scalars <- get_scalars_of_run(data, name_of_run)
+  packets_sent_values <- get_column_values(scalars, "packetSent:count")
+  packets_received_values <- get_column_values(scalars, "packetReceived:count")
+  energy_consumption_values <- get_column_values(scalars, "residualEnergyCapacity:last")
+  packets_sent_total <- sum(packets_sent_values)
+  packets_received_total <- sum(packets_received_values)
+  energy_consumption_total <- sum(energy_consumption_values)
+  statistics <- data.frame(
+    run_name = name_of_run,
+    total_packets_sent = packets_sent_total,
+    total_packets_received = packets_received_total,
+    total_energy_consumtion = energy_consumption_total,
+    packet_received_ratio = packets_received_total/packets_sent_total
+  )
+  return(statistics)
 }
