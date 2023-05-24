7008f7f54a0797c5dc1f1183054a12161e7936d3
KamilLyczko <kamlyczko@gmail.com>
2023-05-24 10:45:05 +0200

    app2 packet receved ts def

--- scripts/802154/functions.R
+++ scripts/802154/functions.R
@@ -1,10 +1,10 @@
 # funkcja zwraca listę z ramkami danych dotyczącymi szeregów czasowych otrzymanych pakietów
 #   wyznaczonych dla podanego okna czasowego
-get_packet_received_ts_dfs <- function(vector_data_list, window_size) {
+get_packet_received_ts_dfs <- function(vector_data_list, packet_rec_index, window_size) {
   packet_received_ts_list <- list()
   for (i in 1:length(vector_data_list)) {
-    packet_received_vec <- vector_data_list[[i]][[2]]$server_packet_received
-    time_vec <- vector_data_list[[i]][[2]]$t
+    packet_received_vec <- vector_data_list[[i]][[packet_rec_index]]$server_packet_received
+    time_vec <- vector_data_list[[i]][[packet_rec_index]]$t
     packet_received_ts_df <- create_packet_received_ts_df(packet_received_vec,
                                                           time_vec,
                                                           window_size)
@@ -37,7 +37,7 @@ create_packet_received_sizes_plots <- function(dfs_list,
                                      "segment",
                                      title,
                                      "czas [s]",
-                                     "liczba pakietów [b]")
+                                     "liczba pakietów [B]")
   }
   return(plot_list)
 }
---
+++ scripts/802154/packet_received_ts_app1_def.R
@@ -0,0 +1,82 @@
+# wczytanie danych związanych z wektorami z plików wynikowych Omnet++
+input_data_dir <- "data_sheets/4_802154/data/original_vectors/app1/"
+file_name_prefix <- "sim802154_app1_N"
+file_nums <- seq(5, 20, 1)
+vectors_names <- c("server_throughput",
+                   "server_packet_received",
+                   "gateway_data_rate",
+                   "server_delay")
+
+vector_data <- load_vector_data(vectors_names,
+                                input_data_dir,
+                                file_name_prefix,
+                                file_nums)
+
+# utworzenie ramek danych z wektorami dotyczącymi pakietów otrzymanych w czasie każdej sekundy
+packet_received_ts_dfs <- get_packet_received_ts_dfs(vector_data, 2, 1)
+
+# zapisanie utworzonych szeregów do plików
+packet_received_dir <- "data_sheets/4_802154/data/packet_received_vectors/app1/"
+save_df_list(packet_received_ts_dfs,
+             packet_received_dir,
+             file_name_prefix,
+             file_nums,
+             "packet_received.csv"
+             )
+
+# wizualizacja liczb otrzymanych pakietów
+network_sizes <- seq(5, 20, 1)
+
+packet_received_plots <- create_packet_received_sizes_plots(packet_received_ts_dfs,
+                                                            network_sizes)
+
+display_plots(packet_received_plots)
+
+packet_received_comp_df <- create_comparison_df(packet_received_ts_dfs,
+                                                network_sizes,
+                                                "total_packets_size")
+
+
+packet_received_comp_range1 <- cbind(packet_received_comp_df[1],
+                                     packet_received_comp_df[, 2:9])
+packet_received_comp_range2 <- cbind(packet_received_comp_df[1],
+                                     packet_received_comp_df[, 10:17])
+comp_plots <- list()
+comp_plots[[1]] <- create_multiple_ts_plot_from_df(packet_received_comp_df,
+                                                   "segment",
+                                                   "Zestawienie liczb otrzymywanych pakietów",
+                                                   "czas [s]",
+                                                   "liczba pakietów [B]",
+                                                   "Liczba sensorów w sieci:")
+
+comp_plots[[2]] <- create_multiple_ts_plot_from_df(packet_received_comp_range1,
+                                                   "segment",
+                                                   "Zestawienie liczb otrzymywanych pakietów",
+                                                   "czas [s]",
+                                                   "liczba pakietów [B]",
+                                                   "Liczba sensorów w sieci:")
+
+comp_plots[[3]] <- create_multiple_ts_plot_from_df(packet_received_comp_range2,
+                                                   "segment",
+                                                   "Zestawienie liczb otrzymywanych pakietów",
+                                                   "czas [s]",
+                                                   "liczba pakietów [B]",
+                                                   "Liczba sensorów w sieci:")
+
+display_plots(comp_plots)
+
+
+
+# czyszczenie środowiska
+rm(input_data_dir)
+rm(file_name_prefix)
+rm(file_nums)
+rm(vectors_names)
+rm(vector_data)
+rm(packet_received_dir)
+rm(network_sizes)
+rm(packet_received_comp_range1)
+rm(packet_received_comp_range2)
+
+
+
